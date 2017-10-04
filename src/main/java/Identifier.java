4de8900dafe49340c16e598aa17a175e899fbef3
rubenvdham <ravdham@gmail.com>
2017-10-04 20:40:04 +0200

    Identifier class and interface work with stringbuffer now, Main uses Scanner throughout the parsing process

--- src/main/java/Identifier.java
+++ src/main/java/Identifier.java
@@ -1,12 +1,62 @@
-public class Identifier implements IdentifierInterface{
+public class Identifier implements IdentifierInterface {
 
-    private String identifier;
+    private StringBuffer identifier;
 
-    Identifier(String name){
-            identifier = name;
+    Identifier() {
+        identifier = new StringBuffer();
     }
 
-    public String getString(){
-        return identifier;
+    @Override
+    public String toString() {
+        return identifier.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return identifier.toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object toCompare) {
+        if (toCompare instanceof Identifier) {
+            if (toCompare.hashCode() == this.hashCode()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean appendValidIdentifier(String toAppend){
+        if (hasCorrectIdentifierFormat(toAppend)){
+            append(toAppend);
+            return true;
+        }
+        return false;
+    }
+
+    private void append(String toAppend){
+        identifier.append(toAppend);
+    }
+
+    private boolean isLetter(char input){
+        return Character.isLetter(input);
+    }
+
+    private boolean isDigit(char input){
+        return Character.isDigit(input);
+    }
+
+    private boolean hasCorrectIdentifierFormat(String input){
+        if(!isLetter(input.charAt(0))){
+            return  false;
+        }
+        for (int i = 1; i < input.length();i++){
+            char currentChar = input.charAt(i);
+            if(!(isLetter(currentChar) || isDigit(currentChar))){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
-
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class Main {
 
     static final String IDENTIFIER_FORMAT_EXCEPTION = "Space in Identifier not allowed";
+    static final String IDENTIFIER_BLANK_EXCEPTION = "An Identifier has to have a name";
 
     Scanner input;
     PrintStream out;
@@ -21,75 +22,74 @@ public class Main {
 
 
 
-    Set parseInput(String statement)throws APException {
-        //Scanner statementScanner = new Scanner(statement);
+    Set parseInput(Scanner input)throws APException {
         Set result = null;
-        if (nextCharIsLetter(statement)){
-            result = parseIdentifier(statement);
+        if (nextCharIsLetter(input)){
+            parseIdentifier(input);
         } else {
-            if (nextCharIsAccolade(statement)){
-                result = parseSet(statement);
+            if (nextCharIs(input,'{')){
+                result = parseSet(input);
             } else {
-                if (nextCharIsParenthesis(statement)) {
-                    result = parseComplexFactor(statement);
+                if (nextCharIs(input,'(')) {
+                    result = parseComplexFactor(input);
                 }
 
             }}
         return result;
     }
 
-    Set parseIdentifier(String statement) throws APException {
-        Scanner statementScanner = new Scanner(statement);
-        statementScanner.useDelimiter("=");
-        if (statementScanner.hasNext()){
-            String identifierName = statementScanner.next();
+    void parseIdentifier(Scanner input) throws APException {
+        input.useDelimiter("=");
+        Scanner IdentifierScanner = new Scanner(input.next());
 
-            if(hasCorrectIdentifierFormat(identifierName)) {
-                Identifier tempID = new Identifier(identifierName);
-                statementScanner.skip("=");
-                Set tempSet = parseInput(statementScanner.next());
-            }else{
-                throw new APException(IDENTIFIER_FORMAT_EXCEPTION);
-            }
+        String identifierName = IdentifierScanner.next();
+        if (identifierName.equals("")) {
+            throw new APException(IDENTIFIER_BLANK_EXCEPTION);
         }
-        //store new id/set combo in hashmap
-        return null;
+
+        Identifier tempID = new Identifier();
+        if(!tempID.appendValidIdentifier(identifierName)) {
+            throw new APException(IDENTIFIER_FORMAT_EXCEPTION);
+        }
+
+        input.skip("=");
+        Set tempSet = parseInput(input);
+
+        //store new id/set combo in hashmap??
     }
 
-    Set parseSet(String statement){
-        Scanner statementScanner = new Scanner(statement);
+    Set parseSet(Scanner input) throws APException{
+        Scanner statementScanner = new Scanner(input.next());
         //parse statement
     }
 
-    Set parseComplexFactor(String statement){
-        Scanner statementScanner = new Scanner(statement);
+    Set parseComplexFactor(Scanner input) throws APException{
+        Scanner statementScanner = new Scanner(input.next());
         //parse statement
     }
 
 
 
-    private boolean nextCharIsLetter(String input){
-        return Character.isLetter(input.charAt(0));
+    private boolean nextCharIsLetter(Scanner input){
+        return input.hasNext("[a-zA-Z]");
     }
-    private boolean nextCharIsAccolade(String input) {
-        return input.charAt(0) == '{';
+   /* private boolean nextCharIsAccolade(Scanner input) {
+        return input.hasNext("[{]");
     }
-    private boolean nextCharIsParenthesis(String input) {
-        return input.charAt(0) == '(';
+    private boolean nextCharIsParenthesis(Scanner input) {
+        return input.hasNext("[(]");
+    }*/
+
+    private boolean nextCharIs(Scanner input, char c){
+        return input.hasNext(Pattern.quote(c+""));
     }
-    private boolean hasCorrectIdentifierFormat(String input){
-        if (Character.isLetter(input.charAt(0)) && !input.contains(" ")){
-            return true;
-        } else {
-            return false;
-        }
-    }
+
 
     private void start() {
         input = new Scanner(System.in);
         while (input.hasNextLine()) {
             try {
-                parseInput(input.nextLine());
+                parseInput(input);
             }catch(APException exception){
                 out.println(exception);
             }
